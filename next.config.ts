66b9e4c2b0776b7588f1b4c5e4599aa4d3a0c446
p.petrunkin <p.petrunkin@norddig.ru>
2026-09-20 21:57:42 +0300

    fix nextjs settings for github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Кардинально важно: включает генерацию статических HTML-файлов
+  images: {
+    unoptimized: true, // Обязательно для github.io, иначе билд упадет на компонентах картинок
+  },
 };
 
 export default nextConfig;
