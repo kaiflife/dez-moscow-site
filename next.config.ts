c162495406a45d1092ccdc6c5a215d4201496cb3
p.petrunkin <p.petrunkin@norddig.ru>
2026-09-20 22:41:54 +0300

    clean cache when deploy

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/dez-moscow-site" : "",
-  assetPrefix: isProd ? "/dez-moscow-site/" : "",
+  basePath: "/dez-moscow-site",
 };
 
 export default nextConfig;
