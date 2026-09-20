60915d4ab7b3d44463c003924090b0162ec67579
p.petrunkin <p.petrunkin@norddig.ru>
2026-09-20 22:36:02 +0300

    fix next config for styles

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/dez-moscow-site",
-  assetPrefix: "/dez-moscow-site/",
+  basePath: isProd ? "/dez-moscow-site" : "",
+  assetPrefix: isProd ? "/dez-moscow-site/" : "",
 };
 
 export default nextConfig;
